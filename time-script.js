39ed05bc7342524f58ff8f49a8e1299af29ee1c6
Chesters-CZ <bezouskavit@gmail.com>
2022-11-15 17:08:34 +0100

    tak prej element.value vždycky vrací string...
    dík, ~~#2468

--- time-script.js
+++ time-script.js
@@ -2,10 +2,11 @@ let interval = window.setInterval(function () {
     let rn = new Date();
     document.getElementById("timeDisplay").innerText = rn.getHours().toString().padStart(2, "0") +
         ":" + rn.getMinutes().toString().padStart(2, "0") + ":" + rn.getSeconds().toString().padStart(2, "0");
-}, 100); // Každých 100ms se aktualizuje čas
+}, 100);
 
 let isOdpocitava = false;
 let casKonceOdpoctu = 0;
+let countdowntimer;
 
 function switchModes() {
     console.log("switching modes")
@@ -24,27 +25,37 @@ function switchModes() {
 }
 
 function startTimer() {
-    // Date.now() nerespektuje timezóny
     let ted = new Date();
 
     // datum plus time input v ms a timezone offset
-    casKonceOdpoctu = new Date(ted.getTime() +
-        timeToMillis(0, 0, // absolutně netušim proč, ale rozbíjí se to, když tu funkci použiju normálně :(
-            document.getElementById("secondInput").value
-            + (document.getElementById("minuteInput").value * 60)
-            + (document.getElementById("hourInput").value * 60 * 60))
+    casKonceOdpoctu = new Date(ted.getTime()
+        + timeToMillis(document.getElementById("hourInput").valueAsNumber,
+            document.getElementById("minuteInput").valueAsNumber,
+            document.getElementById("secondInput").valueAsNumber)
         + timeToMillis(0, ted.getTimezoneOffset(), 0));
 
-    window.setInterval(function () {
-        console.log("------------------------------");
-        console.log(casKonceOdpoctu);
-        console.log(Date.now());
-        console.log(ted.getTimezoneOffset())
+    countdowntimer = window.setInterval(function () {
         let cas = casKonceOdpoctu - Date.now();
         cas = new Date(cas);
         console.log(cas);
         document.getElementById("countdownTimer").innerText = cas.getHours().toString().padStart(2, "0") +
             ":" + cas.getMinutes().toString().padStart(2, "0") + ":" + cas.getSeconds().toString().padStart(2, "0");
+
+        if (document.getElementById("countdownTimer").innerText === "00:00:00") {
+            clearInterval(countdowntimer);
+            casKonceOdpoctu = 8;
+            countdowntimer = window.setInterval(function () {
+                if (casKonceOdpoctu === 0) {
+                    clearInterval(countdowntimer);
+                    document.getElementById("countdownTimer").classList.remove("red");
+                } else if (casKonceOdpoctu % 2 === 1) {
+                    document.getElementById("countdownTimer").classList.add("red");
+                } else {
+                    document.getElementById("countdownTimer").classList.remove("red");
+                }
+                casKonceOdpoctu--;
+            }, 750);
+        }
     }, 50);
 }
 
